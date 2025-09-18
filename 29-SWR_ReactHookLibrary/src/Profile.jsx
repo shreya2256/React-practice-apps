50ca257a9a60ba1f70cccb88f978fe2e49b4861f
Shreya Ganesh Shinde <shreyashinde2256@gmail.com>
2025-09-18 18:10:49 +0530

    Refactor Profile component: update SWR usage and improve loading message

--- 29-SWR_ReactHookLibrary/src/Profile.jsx
+++ 29-SWR_ReactHookLibrary/src/Profile.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import useSWR from 'swr';
+import React from "react";
+import useSWR from "swr";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function Profile() {
-  const { data, error, isLoading } = useSWR('/api/profile', fetcher);
+  //const {data, error, isLoading} =  useSWR('/api/profile', fetcher)
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/users/1",
+    //"https://jsonplaceholder.typicode.com/users/2", here we can change the user id to get different user data
+    fetcher
+  );
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading......</p>;
   if (error) return <p>Failed to load</p>;
 
   return <div>Welcome {data.name}</div>;
 }
 
-export default Profile
+export default Profile;
